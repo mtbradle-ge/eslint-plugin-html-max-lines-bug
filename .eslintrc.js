21791028c9edccf67d0ed333e26abaef84037c26
mtbradle <Matthew.Bradley2@ge.com>
2017-05-18 15:06:37 -0400

    Initial

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'rules': {
+      'max-lines': [2, {
+        'max': 1
+      }],
+      'max-len': [ 2, {
+        'code': 35
+      }]
+    },
+    'plugins': [
+      'html'
+    ]
+};
